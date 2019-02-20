c764430d3bead963ce41450f49ad2569de665533
DANIELKAKAI <danielkakai3@gmail.com>
2019-02-21 00:54:15 +0300

    version 2

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask import request
 #from tweeter_api.sentiments import Sentiment
 from tweeter_api.google_cloud_nlp import Sentiment
-import json
 from flask_cors import CORS
 
 
@@ -22,11 +21,11 @@ def search():
 	if request.method == 'POST':q = request.get_json()['query']
 	if q.startswith("https://"):
 		res = sentiment.replies_analysis(tweet_url=q)
-		return json.dumps({'data':res})
+		return jsonify({'data':res})
 	elif q != '':
 		res = sentiment.tweets_analysis(query=q)
-		return json.dumps({'data':res})
-	return json.dumps({'data':[0,0,0]})
+		return jsonify({'data':res})
+	return jsonify({'data':[0,0,0]})
 
 
 @app.errorhandler(500)
@@ -38,5 +37,7 @@ def page_not_found(error):
 	return 404
 
 
+'''
 if __name__=='__main__':
 	app.run(debug=True, port=5000)
+'''
